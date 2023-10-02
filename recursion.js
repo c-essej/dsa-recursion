c1737afc28b343de9415cff43003883d65c50cb5
Jason Johnson <jasjoh@live.com>
2023-10-02 13:06:41 -0700

    completed findIndex()

--- recursion.js
+++ recursion.js
@@ -55,10 +55,41 @@ function revString(str) {
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
-  if (arr.length === 0) return -1;
-  if (arr[0] === val) return 0;
-
+  if (arr.length === 0) return -1; // reached the end, return -1
+  if (arr[0] === val) return 0 // don't keep looking
+  let nextVal = findIndex(arr.slice(1), val); // check next value
+  if (nextVal === -1 ) return -1; // if it's -1, nothing was found, return that
+  return nextVal + 1; // add 1 + the next value
 }
+
+/**
+ * arr = ['p']
+ * val = 'p'
+ * arr.length = 1
+ * arr[0] = 'p'
+ * return 0
+ *
+ * ==================
+ *
+ * arr = ['c', 'p']
+ * val = 'p'
+ * arr.length = 2
+ * arr[0] = 'c'
+ * findIndex(['p'], 'p') = 0
+ * nextVal = 0
+ * return 1
+ *
+ * ==================
+ *
+ * arr = ['d', 'c', 'p]
+ * val = 'p'
+ * arr.length = 3
+ * arr[0] = 'd'
+ * findIndex(['c', 'p'], 'p') = 1
+ * nextVal = 1
+ * return 2
+ *
+ */
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
